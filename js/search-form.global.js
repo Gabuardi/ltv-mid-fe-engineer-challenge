5efaa8bf9cbd106b3ef573483fae97f129c497a6
Josué Gabuardi <jgabuardi@beenverified.com>
2021-03-19 19:04:21 -0600

    Implement form switch logic

--- js/search-form.global.js
+++ js/search-form.global.js
@@ -1,8 +1,15 @@
 $(function () {
   // --------------------- INIT ---------------------------------------------
+  var $formSwitchButtons = $('.js-form-switch-btn');
+  var $formSwitchTargets = $('.js-form-switch-target');
+  $formSwitchButtons.click(formSwitchTriggerClickHandler);
   utils.setFormValidation('#email-form', formSubmitHandler);
-  utils.setFormValidation('#phone-form', formSubmitHandler)
+  utils.setFormValidation('#phone-form', formSubmitHandler);
   // -------------------------------------------------------------------------
+  function formSwitchTriggerClickHandler() {
+    $formSwitchButtons.toggleClass('active')
+    $formSwitchTargets.toggleClass('d-none');
+  }
 
   function formSubmitHandler(formValue) {
     localStorage.clear();
